88e7cf273701cbfa0d3712b896905dcb724a457e
fandi23 <157145785+fandi321@users.noreply.github.com>
2024-01-25 16:49:42 +0700

    Add files via upload

---
+++ projek-3/shipping-service/build_push_image_shipping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image untuk frontend
+docker build -t fandi23/shipping-service:latest .
+
+# Login ke Docker Hub
+docker login -u fandi23 -p andhy060402
+
+# Push image ke Docker Hub
+docker push fandi23/shipping-service:latest
